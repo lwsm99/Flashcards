49f5781cde72371b415ce77523d3677cdf62dcc2
luizashishina <luiza.shishina@mni.thm.de>
2022-06-20 13:10:01 +0200

    Add colors as Extention

---
+++ Shared/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  Flashcards
+//
+//  Created by Luiza Shishina on 20.06.22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let primary = Color("Primary")
+    static let error = Color("Error")
+    static let statisticsPrimary = Color("StatisticsPrimary")
+    static let statisticsSecondary = Color("StatisticsSecondary")
+    static let statisticsTertiary = Color("StatisticsTertiary")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}

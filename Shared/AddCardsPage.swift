c387380bd01f5de11572ce1613a71f7d53f40583
luizashishina <luiza.shishina@mni.thm.de>
2022-06-21 16:39:54 +0200

    Add skeleton for AddCards and LearnCards pages

---
+++ Shared/AddCardsPage.swift
@@ -0,0 +1,15 @@
+//
+//  AddCardsPage.swift
+//  Flashcards (iOS)
+//
+//  Created by Luiza Shishina on 21.06.22.
+//
+
+import Foundation
+import SwiftUI
+
+struct AddCardsPage: View {
+    var body: some View {
+        Text("TODO: Add AddCardsPage")
+    }
+}

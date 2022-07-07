c3787a4bfae0bf721c1a85ee2fdb1707bb17c3aa
luizashishina <luiza.shishina@mni.thm.de>
2022-07-07 15:47:10 +0200

    Change functionality for add cards and decks on AddCards page

--- Shared/Pages/AddCardsPage.swift
+++ Shared/Pages/AddCardsPage.swift
@@ -19,21 +19,17 @@ struct AddCardsPage: View {
     @State private var bgColor = Color.error
     @State var draggeddIndex = 0
     
-    // Card Variables
-    @State private var cardFront = ""
-    @State private var cardBack = ""
-    @State private var deckOfCard: Deck? = nil
-    
     // Deck Variables
-    @State private var deckTitle = ""
+    @State private var deckTitle = DeckSettings.value?.title
     @State private var deckColor = UIColor.blue // TODO: Change Defaultcolor
     @State private var cardsOfDeck:[Card] = []
+
     
     var body: some View {
         VStack {
             HStack {
                 // Input for deck title
-                TextField("Deck Titel", text: $deckTitle).frame(height: 47)
+                TextField("Deck Titel", text: Binding(get: {return deckTitle ?? ""}, set: {DeckSettings.value != nil ? changeTitle(title: $0) : saveDeck(title: $0)})).frame(height: 47)
                     .padding([.leading, .trailing], 15)
                     .background(Color.white, in: Capsule())
                 
@@ -42,47 +38,60 @@ struct AddCardsPage: View {
             ScrollView {
             
                 ForEach(
-                    0...1,
+                    cardsOfDeck.indices,
                     id: \.self
                 ) {
                     card in
                     ZStack(alignment: .trailing) {
                         
                         // Delete button
-                        Button(action: {}) {
+                        Button(action: {
+                            cardsOfDeck.remove(at: card)
+                            cardPosition = 0
+                        }) {
                             Image(systemName: "trash").font(.system(size: 26)).foregroundColor(Color.white).frame(width: smallButtonSize, height: smallButtonSize).background(Color.error).cornerRadius(smallButtonSize/2)
                         }.padding(.trailing, 10)
                         // Card
-                        VStack {
-                            TextField("Begriff", text: $cardFront).frame(height: 40)
-                                .padding([.leading, .trailing], 15)
-                                .background(Color.white, in: Capsule())
-                            TextField("Definition", text: $cardBack).frame(height: 40)
-                                .padding([.leading, .trailing], 15)
-                                .background(Color.white, in: Capsule())
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding(20)
-                        .background(Color.uncreatedCardColor)
-                        .cornerRadius(20)
-                        .offset(x: getOffset(index: card))
-                        .gesture(DragGesture()
-                            .onChanged({
-                                value in
-                                draggeddIndex = card
-                                if(value.translation.width < 0) {
-                                    cardPosition = -90
-                                }
-                            })
-                            .onEnded(onEnd(value:)))
-                        .animation(.spring(), value: getOffset(index: card))
+                    
+                            VStack {
+                                TextField("Begriff", text: Binding(get: {return cardsOfDeck[card].front ?? ""}, set: {saveCardFront(card: cardsOfDeck[card], val: $0)}))
+                                    .frame(height: 40)
+                                    .padding([.leading, .trailing], 15)
+                                    .background(Color.white, in: Capsule())
+                                TextField("Definition", text: Binding(get: {return cardsOfDeck[card].back ?? ""}, set: {saveCardBack(card: cardsOfDeck[card], val: $0)}))
+                                    .frame(height: 40)
+                                    .padding([.leading, .trailing], 15)
+                                    .background(Color.white, in: Capsule())
+                            }
+                                .frame(maxWidth: .infinity)
+                                .padding(20)
+                                .background(Color.uncreatedCardColor)
+                                .cornerRadius(20)
+                                .offset(x: getOffset(index: card))
+                                .gesture(DragGesture()
+                                    .onChanged({
+                                        value in
+                                        draggeddIndex = card
+                                        if(value.translation.width < 0) {
+                                            cardPosition = -90
+                                        }
+                                    })
+                                    .onEnded(onEnd(value:)))
+                                .animation(.spring(), value: getOffset(index: card))
+                        
+                        
+                        
                     }
                 }.padding(.bottom, 10)
+            }.onAppear {
+                if ( DeckSettings.value == nil ) {
+                    DeckSettings.value = initDeck()
+                    cardsOfDeck.append(initCard())
+                }
+
             }
             Button(action: {
-                print("Save")
-                //addDeck()
-                //addCard()
+                cardsOfDeck.append(initCard())
             }){
                 Image(systemName: "plus").font(.system(size: 20)).foregroundColor(Color.primary).frame(width: smallButtonSize, height: smallButtonSize).background(Color.white).cornerRadius(smallButtonSize/2)
             }
@@ -106,28 +115,34 @@ struct AddCardsPage: View {
         }
     }
     
-    private func addDeck() {
-        let deck = Deck(context: viewContext)
-        
+    func saveDeck(title: String) {
         // Initialize Data
-        deck.id = UUID()
-        deck.title = deckTitle
-        deck.color = deckColor
+            DeckSettings.value?.title = title
+            DeckSettings.value?.color = deckColor
+            
+            
+            print("cards:::", cardsOfDeck)
+            
+            for card in cardsOfDeck {
+                card.cardToDeck = DeckSettings.value
+                DeckSettings.value?.addToDeckToCard(card)
+            }
+            
+            
+        print("Trying to save the deck", DeckSettings.value!)
+            // Try saving
+            do {
+                try viewContext.save()
+                print("Saving success")
+                DeckSettings.value = nil
+            } catch {
+                print("Unexpected error: \(error.localizedDescription).")
+            }
         
-        // Assign Cards to deck
-        let uniqueCard = Set(cardsOfDeck)
-        for card in uniqueCard {
-            deck.addToDeckToCard(card)
-        }
-        
-        // Set Deck of card
-        deckOfCard = deck
-        
-        // Try saving
-        try? viewContext.save()
     }
     
-    private func addCard() {
+    
+    private func initCard() -> Card {
         let card = Card(context: viewContext)
         
         // Initialize Data
@@ -138,17 +153,29 @@ struct AddCardsPage: View {
         card.failedCount = 0
         card.lastReviewed = card.createdAt
         
-        // Set Front/Back of the card to the value assigned in the view
-        card.front = cardFront
-        card.back = cardBack
+        card.cardToDeck = DeckSettings.value
         
-        // Set Deck for the card
-        card.cardToDeck = deckOfCard
-        
-        // Append Card to relationship
-        cardsOfDeck.append(card)
-        
-        // Try saving
-        try? viewContext.save()
+        return card
     }
+    
+    private func initDeck() -> Deck {
+        let deck = Deck(context: viewContext)
+        deck.id = UUID()
+        
+        return deck
+    }
+    
+    private func saveCardFront(card: Card, val: String) {
+        card.front = val
+    }
+    
+    private func saveCardBack(card: Card, val: String) {
+        card.back = val
+    }
+    
+    private func changeTitle(title: String) {
+        DeckSettings.value?.title = title
+    }
+    
+    
 }
